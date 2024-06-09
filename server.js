38b90cb031db7e84d26c34488e551b5eb61e084a
Mashenkin Oleg <71937799+whiterabbit02@users.noreply.github.com>
2024-06-10 03:27:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
+const authenticateToken = require('./middleware/authenticateToken'); // Убедитесь, что этот файл существует
+const multer = require('multer');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const winston = require('winston');
-const authenticateToken = require('./middleware/authenticateToken');
-const multer = require('multer');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,26 +24,18 @@ const logger = winston.createLogger({
 });
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'ep-bold-pine-a4y0h4wd-pooler.us-east-1.aws.neon.tech',
-  database: process.env.DB_NAME || 'smartnote',
-  password: process.env.DB_PASSWORD || '123123890olkAa',
-  port: process.env.DB_PORT || 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
-// Настройка CORS
-const corsOptions = {
-  origin: 'http://185.173.94.160',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.options('*', cors(corsOptions)); // включаем pre-flight запросы для всех маршрутов
+app.options('*', cors()); // включаем pre-flight запросы для всех маршрутов
 
 app.get('/', (req, res) => {
   res.send('Server is running');
