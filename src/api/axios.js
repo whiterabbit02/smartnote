c71518f569fc1b428d125bb96eb704cfae71a3f5
Mashenkin Oleg <71937799+whiterabbit02@users.noreply.github.com>
2024-06-10 03:22:10 +0700

    axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://185.173.94.160:5000/', // Используем относительный путь
+  baseURL: process.env.REACT_APP_API_URL, // Используем относительный путь
   headers: {
     'Content-Type': 'application/json',
   }
